75bc4ec2b33db66d3dcd8e1977c81cbc94b0e4b5
ebitender <ebitender@gmail.com>
2023-06-11 13:14:56 +0900

    Update: Property simplify

--- Assets/H17CubemapGenerator/Runtime/Scripts/Utilities/Property.cs
+++ Assets/H17CubemapGenerator/Runtime/Scripts/Utilities/Property.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -9,241 +9,112 @@ using UnityEngine;
 
 namespace Hoshino17
 {
-	public class PropertyBool
+	public class PropertyBase<T>
 	{
-		string? _prefsKey;
-		public Action<bool>? onValueChanged;
-
-		public PropertyBool(string? prefsKey, bool def = default)
-		{
-			_prefsKey = prefsKey;
-			_value = def;
-			if (!string.IsNullOrEmpty(_prefsKey))
-			{
-				_value = PropertyKeyValue.LoadBool(_prefsKey, def);
-			}
-		}
-
-		bool _value;
-		public bool value
-		{
-			get => _value;
-			set
-			{
-				if (_value != value)
-				{
-					_value = value;
-					if (!string.IsNullOrEmpty(_prefsKey))
-					{
-						PropertyKeyValue.SaveBool(_prefsKey, value);
-					}
-					onValueChanged?.Invoke(value);
-				}
-			}
-		}
-	}
-
-	public class PropertyInt
-	{
-		string? _prefsKey;
-		public Action<int>? onValueChanged;
-
-		public PropertyInt(string? prefsKey, int def = default)
-		{
-			_prefsKey = prefsKey;
-			_value = def;
-			if (!string.IsNullOrEmpty(_prefsKey))
-			{
-				_value = PropertyKeyValue.LoadInt(_prefsKey, def);
-			}
-		}
-
-		int _value;
-		public int value
-		{
-			get => _value;
-			set
-			{
-				if (_value != value)
-				{
-					_value = value;
-					if (!string.IsNullOrEmpty(_prefsKey))
-					{
-						PropertyKeyValue.SaveInt(_prefsKey, value);
-					}
-					onValueChanged?.Invoke(value);
-				}
-			}
-		}
-	}
-
-	public class PropertyFloat
-	{
-		string? _prefsKey;
-		public Action<float>? onValueChanged;
-
-		public PropertyFloat(string? prefsKey, float def = default)
-		{
-			_prefsKey = prefsKey;
-			_value = def;
-			if (!string.IsNullOrEmpty(_prefsKey))
-			{
-				_value = PropertyKeyValue.LoadFloat(_prefsKey, def);
-			}
-		}
-
-		float _value;
-		public float value
-		{
-			get => _value;
-			set
-			{
-				if (_value != value)
-				{
-					_value = value;
-					if (!string.IsNullOrEmpty(_prefsKey))
-					{
-						PropertyKeyValue.SaveFloat(_prefsKey, value);
-					}
-					onValueChanged?.Invoke(value);
-				}
-			}
-		}
-	}
-
-	public class PropertyString
-	{
-		string? _prefsKey;
-		public Action<string>? onValueChanged;
-
-		public PropertyString(string? prefsKey, string def = "")
-		{
-			_prefsKey = prefsKey;
-			_value = def;
-			if (!string.IsNullOrEmpty(_prefsKey))
-			{
-				_value = PropertyKeyValue.LoadString(_prefsKey, def);
-			}
-		}
-
-		string _value;
-		public string value
-		{
-			get => _value;
-			set
-			{
-				if (_value != value)
-				{
-					_value = value;
-					if (!string.IsNullOrEmpty(_prefsKey))
-					{
-						PropertyKeyValue.SaveString(_prefsKey, value);
-					}
-					onValueChanged?.Invoke(value);
-				}
-			}
-		}
-	}
-
-	public class PropertyEnum<T> where T : struct
-	{
+		T? _value;
 		string? _prefsKey;
 		public Action<T>? onValueChanged;
+		public Action<string, T?>? onSaveValue;
 
+		public PropertyBase(string? prefsKey, T? def = default(T), Func<string, T?, T?>? onLoadValue = null, Action<string, T?>? onSaveValue = null)
+		{
+			_prefsKey = prefsKey;
+			if (!string.IsNullOrEmpty(_prefsKey) && onLoadValue != null)
+			{
+				_value = onLoadValue.Invoke(_prefsKey, def);
+			}
+			else
+			{
+				_value = def;
+			}
+			this.onSaveValue = onSaveValue;
+		}
+
+		public T? value
+		{
+			get => _value;
+			set
+			{
+				if (!IEquatable<bool>.Equals(_value, value))
+				{
+					_value = value;
+					onValueChanged?.Invoke(value);
+					if (!string.IsNullOrEmpty(_prefsKey))
+					{
+						onSaveValue?.Invoke(_prefsKey, value);
+					}
+				}
+			}
+		}
+	}
+
+	public class PropertyBool : PropertyBase<bool>
+		{
+		public PropertyBool(string? prefsKey, bool def = default)
+			: base(prefsKey, def,
+			onLoadValue: (prefsKey_, def_) => PropertyKeyValue.LoadBool(prefsKey_, def_),
+			onSaveValue: (prefsKey_, value_) => PropertyKeyValue.SaveBool(prefsKey_, value_))
+		{}
+	}
+
+	public class PropertyInt : PropertyBase<int>
+	{
+		public PropertyInt(string? prefsKey, int def = default)
+			: base(prefsKey, def,
+			onLoadValue: (prefsKey_, def_) => PropertyKeyValue.LoadInt(prefsKey_, def_),
+			onSaveValue: (prefsKey_, value_) => PropertyKeyValue.SaveInt(prefsKey_, value_))
+		{}
+	}
+
+	public class PropertyFloat : PropertyBase<float>
+	{
+		public PropertyFloat(string? prefsKey, float def = default)
+			: base(prefsKey, def,
+			onLoadValue: (prefsKey_, def_) => PropertyKeyValue.LoadFloat(prefsKey_, def_),
+			onSaveValue: (prefsKey_, value_) => PropertyKeyValue.SaveFloat(prefsKey_, value_))
+		{}
+	}
+
+	public class PropertyString : PropertyBase<string>
+	{
+		public PropertyString(string? prefsKey, string def = "")
+			: base(prefsKey, def,
+			onLoadValue: (prefsKey_, def_) => PropertyKeyValue.LoadString(prefsKey_, def_),
+			onSaveValue: (prefsKey_, value_) => PropertyKeyValue.SaveString(prefsKey_, value_))
+		{ }
+	}
+
+	public class PropertyEnum<T> : PropertyBase<T> where T : struct
+	{
 		public PropertyEnum(string? prefsKey, T def = default(T))
-		{
-			_prefsKey = prefsKey;
-			_value = def;
-			if (!string.IsNullOrEmpty(_prefsKey))
-			{
-				_value = (T)(object)PropertyKeyValue.LoadInt(_prefsKey, (int)(object)def);
-			}
-		}
-
-		T _value;
-		public T value
-		{
-			get => _value;
-			set
-			{
-				if (!IEquatable<T>.Equals(_value, value))
-				{
-					_value = value;
-					if (!string.IsNullOrEmpty(_prefsKey))
-					{
-						PropertyKeyValue.SaveInt(_prefsKey, (int)(object)value);
-					}
-					onValueChanged?.Invoke(value);
-				}
-			}
-		}
+			: base(prefsKey, def,
+			onLoadValue: (prefsKey_, def_) => (T)(object)PropertyKeyValue.LoadInt(prefsKey_, (int)(object)def_),
+			onSaveValue: (prefsKey_, value_) => PropertyKeyValue.SaveInt(prefsKey_, (int)(object)value_))
+		{ }
 	}
 
-	public class PropertyAsset<T> where T : UnityEngine.Object
+	public class PropertyAsset<T> : PropertyBase<T> where T : UnityEngine.Object
 	{
-		string? _prefsKey;
-		public Action<T?>? onValueChanged;
-
 		public PropertyAsset(string? prefsKey, T? def = default(T))
-		{
-			_prefsKey = prefsKey;
-			_value = def;
-			if (!string.IsNullOrEmpty(_prefsKey))
-			{
+			: base(prefsKey, def,
+			onLoadValue: (prefsKey_, def_) => (T)AssetDatabase.LoadAssetAtPath<T>(PropertyKeyValue.LoadString(prefsKey_, string.Empty)),
+			onSaveValue: (prefsKey_, value_) => {
+				string str = string.Empty;
 #if UNITY_EDITOR
-				_value = (T)AssetDatabase.LoadAssetAtPath<T>(PropertyKeyValue.LoadString(_prefsKey, string.Empty));
+				if (value_ != null) { str = AssetDatabase.GetAssetPath(value_); }
 #endif
-			}
-		}
-
-		T? _value;
-		public T? value
-		{
-			get => _value;
-			set
-			{
-				if (!IEquatable<T>.Equals(_value, value))
-				{
-					_value = value;
-					if (!string.IsNullOrEmpty(_prefsKey))
-					{
-						string str = string.Empty;
-						if (_value != null)
-						{
-#if UNITY_EDITOR
-							str = AssetDatabase.GetAssetPath(_value);
-#endif
-						}
-						PropertyKeyValue.SaveString(_prefsKey, str);
-					}
-					onValueChanged?.Invoke(value);
-				}
-			}
-		}
+				PropertyKeyValue.SaveString(prefsKey_, str);
+			})
+		{ }
 	}
 
-	public class PropertyObject<T> where T : UnityEngine.Object
+	public class PropertyObject<T> : PropertyBase<T> where T : UnityEngine.Object
 	{
-		public Action<T?>? onValueChanged;
-
-		public PropertyObject(T? def = default(T))
-		{
-			_value = def;
-		}
-
-		T? _value;
-		public T? value
-		{
-			get => _value;
-			set
-			{
-				if (!IEquatable<T>.Equals(_value, value))
-				{
-					_value = value;
-					onValueChanged?.Invoke(value);
-				}
-			}
-		}
+		public PropertyObject(string? prefsKey, T? def = default(T))
+			: base(prefsKey, def,
+			onLoadValue: null,
+			onSaveValue: null)
+		{ }
 	}
 
 	public static class PropertyKeyValue
@@ -268,6 +139,21 @@ namespace Hoshino17
 			PlayerPrefs.SetString(key, value);
 		}
 
+		public static void SaveVector2(string key, Vector2 value)
+		{
+			PlayerPrefs.SetString(key, $"{value.x},{value.y}");
+		}
+
+		public static void SaveVector3(string key, Vector3 value)
+		{
+			PlayerPrefs.SetString(key, $"{value.x},{value.y},{value.z}");
+		}
+
+		public static void SaveVector4(string key, Vector4 value)
+		{
+			PlayerPrefs.SetString(key, $"{value.x},{value.y},{value.z},{value.w}");
+		}
+
 		public static int LoadInt(string key, int defaultValue)
 		{
 			return PlayerPrefs.GetInt(key, defaultValue);
@@ -286,6 +172,55 @@ namespace Hoshino17
 		public static string LoadString(string key, string defaultValue)
 		{
 			return PlayerPrefs.GetString(key, defaultValue);
+		}
+
+		static void LoadVectorAndSplit<T>(ref T x, string key, Func<string[], T, T> onSplit)
+		{
+			string str = LoadString(key, string.Empty);
+			if (!string.IsNullOrEmpty(str))
+			{
+				string[] strs = str.Split(',');
+				x = onSplit.Invoke(strs, x);
+			}
+		}
+
+		public static Vector2 LoadVector2(string key, Vector2 defaultValue)
+		{
+			Vector2 tmp = defaultValue;
+			LoadVectorAndSplit<Vector2>(ref tmp, key, onSplit: (strs, tmp) =>
+			{
+				float.TryParse(strs[0], out tmp.x);
+				float.TryParse(strs[1], out tmp.y);
+				return tmp;
+			});
+			return tmp;
+		}
+
+		public static Vector3 LoadVector3(string key, Vector3 defaultValue)
+		{
+			Vector3 tmp = defaultValue;
+			LoadVectorAndSplit<Vector3>(ref tmp, key, onSplit: (strs, tmp) =>
+			{
+				float.TryParse(strs[0], out tmp.x);
+				float.TryParse(strs[1], out tmp.y);
+				float.TryParse(strs[2], out tmp.z);
+				return tmp;
+			});
+			return tmp;
+		}
+
+		public static Vector4 LoadVector4(string key, Vector4 defaultValue)
+		{
+			Vector4 tmp = defaultValue;
+			LoadVectorAndSplit<Vector4>(ref tmp, key, onSplit: (strs, tmp) =>
+			{
+				float.TryParse(strs[0], out tmp.x);
+				float.TryParse(strs[1], out tmp.y);
+				float.TryParse(strs[2], out tmp.z);
+				float.TryParse(strs[3], out tmp.w);
+				return tmp;
+			});
+			return tmp;
 		}
 	}
 }
